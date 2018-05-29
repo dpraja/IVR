baac198fbe22bca45f5da524b4e7808688470467
dpraja <raja.pushpanathan@gmail.com>
2018-05-29 20:29:42 +0530

    Update RoomList.py

--- RoomList.py
+++ RoomList.py
@@ -3,7 +3,7 @@ from sqlwrapper import gensql,dbfetch,dbget
 
 def roomlist(request):
     business_id = request.json['business_id']
-    res = json.loads(dbget("select room_type,standard_rate,totel_room,facilitie1,facilitie2,facilitie3 from extranet_room_list where business_id='"+business_id+"' "))
+    res = json.loads(dbget("select * from extranet_room_list where business_id='"+business_id+"' "))
     return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success","Room_List":res},indent=2))
 
 
