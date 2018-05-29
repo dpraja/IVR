c933d03cca318976f74ea20b46545c0f92863f43
dpraja <raja.pushpanathan@gmail.com>
2018-05-29 21:24:27 +0530

    Update RoomList.py

--- RoomList.py
+++ RoomList.py
@@ -5,6 +5,19 @@ def roomlist(request):
     business_id = request.json['business_id']
     res = json.loads(dbget("select * from extranet_room_list where business_id='"+business_id+"' "))
     return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success","Room_List":res},indent=2))
+def insertroomlist(request):
+  try:  
+    d = request.json
+    print(d)
+    no = json.loads(dbget("select count(*) from extranet_room_list"))
+    print(no[0]['count'])
+    id1 = no[0]['count']+1
+    print(id1)
+    d['id']= id1
+    print(gensql('insert','extranet_room_list',d))
+    return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success"},indent=2))
+  except:
+     return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Failure"},indent=2)) 
 
 
 
