51286f1e8dd67b7ffb768b04380d3761543187ec
dpraja <raja.pushpanathan@gmail.com>
2018-06-09 15:45:44 +0530

    Update QueryDiscount.py

--- QueryDiscount.py
+++ QueryDiscount.py
@@ -4,10 +4,38 @@ from sqlwrapper import gensql,dbfetch,dbget
 
 def querydiscount(request):
     res = request.json
-    d = { k : v for k,v in res.items() if k not in ('room_date')} 
-    res_id = json.loads(gensql('select','extranet_room_list','id',d))
-    res_id = str(res_id[0]['id'])
+    print(res)
+    d = { k : v for k,v in res.items() if k not in ('room_date')}
+    print(d)
+    res_id = json.loads(gensql('select','extranet_room_list','id,room_type',d))
     print(res_id)
-    result = json.loads(dbget("select room_date,discount_type,discount_rate,discount_in,discount_on  from extranet_discount where id in ('"+res_id+"') and room_date = '"+res['room_date']+"'"))
-    print(result,type(result))
-    return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success","Result":result},indent=2))
+    id1 = ''
+    room_type = []
+    for i in res_id:
+        print(i)
+        if len(id1) == 0:
+            id1 += "'"+str(i['id'])+"'"
+            print(id1)
+        else:
+            id1 += ','+"'"+str(i['id'])+"'"
+    print(room_type,type(room_type))
+    print(id1)
+    result = json.loads(dbget("select *  from extranet_discount where id in ("+id1+") and room_date = '"+res['room_date']+"'"))
+    print("resutl",result,type(result))
+    e = { k : v for k,v in result[0].items() if k not in ('id')}
+    print(e)
+    id2 = ''
+    for i in result:
+        if len(id2) == 0:
+           id2 += "'"+str(i['id'])+"'"
+        else:
+           id2 += ','+"'"+str(i['id'])+"'"
+    print(id2)
+    res = json.loads(dbget("select room_type from  extranet_room_list where id in ("+id2+")"))
+    print(res,type(res))
+    for j in res:
+        room_type.append(""+str(j['room_type'])+"")
+    
+    e['room_type'] = room_type
+    print(e)
+    return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success","Result":e},indent=2))
