f15028f1b89b355e6bcdef28f883a9f2e3923c86
dpraja <raja.pushpanathan@gmail.com>
2018-06-09 16:38:40 +0530

    Update AddDiscount.py

--- AddDiscount.py
+++ AddDiscount.py
@@ -5,11 +5,14 @@ from sqlwrapper import gensql,dbfetch,dbget
 def adddiscount(request):
     res = request.json
     print(res)
-    e = { k : v for k,v in res.items() if k in ('business_id','room_type','room_name')}
-    d = { k : v for k,v in res.items() if k not in ('business_id','room_type','room_name')}
-    
+    e = { k : v for k,v in res.items() if k in ('business_id','room_type')}
+    d = { k : v for k,v in res.items() if k not in ('business_id','room_type')}
+    #print(e)
+    #print(d)
+    f = { k : v for k,v in res.items() if k in ('room_date')}
+    #print("f",f)
     id_data = json.loads(gensql('select','extranet_room_list','id',e))
-    print(id_data,type(id_data))
+    print("id_data",id_data,type(id_data))
     str_id = ''
     for i in id_data:
        sql = json.loads(dbget("select count(*) from extranet_discount where id in ("+str(i['id'])+") and room_date='"+d['room_date']+"' "))
@@ -19,9 +22,15 @@ def adddiscount(request):
             d['id'] = i['id']   
             print(gensql('insert','extranet_discount',d))
             print("inserted")
-            #return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Record Inserted"},indent=2))    
           except:
             return(json.dumps({"ServiceStatus":"Failure","ServiceMessage":"Failure"},indent=2))
        else:
-           print("record already inserted")
+          try: 
+            f['id'] = i['id']   
+            print(gensql('update','extranet_discount',d,f))
+            print("updated")
+          except:
+            return(json.dumps({"ServiceStatus":"Failure","ServiceMessage":"Failure"},indent=2))
+            print("record already inserted")
     return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Record Inserted"},indent=2))   
+    
