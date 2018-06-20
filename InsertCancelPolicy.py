a0cd2f8dabb85590a02cf9c99a59413772545ff0
dpraja <raja.pushpanathan@gmail.com>
2018-06-20 21:43:04 +0530

    Update InsertCancelPolicy.py

--- InsertCancelPolicy.py
+++ InsertCancelPolicy.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import calendar
 from sqlwrapper import gensql,dbget,dbput
 def insertcancelpolicy(request):
     d = request.json
@@ -14,3 +15,31 @@ def insertcancelpolicy(request):
     print(gensql('insert','cancel_policy',d))
     return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success"},indent=2))
     
+def QueryStatistics(request):
+    sql = json.loads(dbget("SELECT customer_arrival_date FROM public.ivr_room_customer_booked where \
+                            customer_arrival_date < '2018-06-20' \
+                            order by customer_arrival_date desc"))
+    print(sql)
+    today = datetime.datetime.utcnow().date()
+    print(today,today.month)
+    a,b,c = 0,0,0
+    d={} 
+    for i in sql:
+        #print(i['customer_arrival_date'],type(i['customer_arrival_date']))
+        i = datetime.datetime.strptime(i['customer_arrival_date'], "%Y-%m-%d").date()
+        
+        #print(i,type(i))
+        if i.month == today.month:
+            a += 1
+            #print (calendar.month_name[today.month])    
+            #print(calendar.i[today.month], "-", calendar.i[today.month])
+        elif i.month == today.month-1:
+            b += 1
+        elif i.month == today.month-2:
+            c += 1
+    d[""+calendar.month_name[today.month]+""+"-"+""+str(i.year)+""] = a
+    d[""+calendar.month_name[today.month-1]+""+"-"+""+str(i.year)+""] = b
+    d[""+calendar.month_name[today.month-2]+""+"-"+""+str(i.year)+""] = c
+    
+    print(a,b,c,d)        
+    return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success","Result":sql},indent=2))
