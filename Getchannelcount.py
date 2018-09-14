dc3615a01ccac5703708c310523343657c24f1d4
dpraja <raja.pushpanathan@gmail.com>
2018-09-14 15:20:12 +0530

    Add files via upload

---
+++ Getchannelcount.py
@@ -0,0 +1,24 @@
+from sqlwrapper import gensql,dbget,dbput
+import json
+import datetime
+import request
+def Getchannelcounts(request):
+    
+    
+    date_from = request.json['arrival_from']
+    date_to = request.json['arrival_to']
+
+
+    channelchatbot = json.loads(dbget("select count(*) from ivr_resevation where arrival_date between  '"+date_from+"' and  '"+date_to+"' and channel in ('CHATBOT')"))
+    print(channelchatbot)
+
+    channelivr = json.loads(dbget("select count(*) from ivr_resevation where arrival_date between  '"+date_from+"' and  '"+date_to+"' and channel in ('IVR')"))
+    print(channelivr)
+
+    ivrcount = json.loads(dbget("select count (*) from ivr_room_customer_booked where customer_arrival_date between  '"+date_from+"' and  '"+date_to+"' and channel in ('IVR')"))
+    print(ivrcount)
+    return(json.dumps({"Return":"Record Retrieved Sucessfully","Return_Code":"RTS","Status": "Success",
+                      "Status_Code": "200","chatbotcount":channelchatbot[0]['count'],
+                       "ivrchannelcount":channelivr[0]['count'] + ivrcount[0]['count']},indent=2))
+    
+    
---
+++ Getreservationcancelmodification.py
@@ -0,0 +1,31 @@
+from sqlwrapper import gensql,dbget,dbput
+import json
+import datetime
+import request
+def Getreservationcancelmodification(request):
+    
+    
+    date_from = request.json['arrival_from']
+    date_to = request.json['arrival_to']
+    sql_value = json.loads(dbget("select * from public.ivr_resevation where arrival_date between  '"+date_from+"' and  '"+date_to+"'"))
+    print(sql_value)
+
+    reservationcount = json.loads(dbget("select count(*) from public.ivr_resevation where arrival_date between '"+date_from+"' and '"+date_to+"'"))
+    print(reservationcount)
+
+    ivreservationcount = json.loads(dbget("select count(*) from public.ivr_room_customer_booked where customer_arrival_date between '"+date_from+"' and '"+date_to+"' and customer_booked_status in ('booked')"))
+    print(ivreservationcount)
+
+    cancelcount = json.loads(dbget("select count(*) from public.ivr_room_customer_booked where customer_arrival_date between '"+date_from+"' and '"+date_to+"' and customer_booked_status in ('canceled')"))
+    print(cancelcount)
+
+    Totalreservationcount = json.loads(dbget("select count(*) from public.ivr_resevation "))
+    print(Totalreservationcount)
+
+    
+    totalivrcount = json.loads(dbget("select count (*) from public.ivr_room_customer_booked"))
+    
+    return(json.dumps({"Return":"Record Retrieved Sucessfully","Return_Code":"RTS","Status": "Success",
+                      "Status_Code": "200","Returnvalue":sql_value,"reservationcount":reservationcount[0]['count'] + ivreservationcount[0]['count'],
+                       "cancelcount":cancelcount[0]['count'],"Totalbookingcount":Totalreservationcount[0]['count'] + totalivrcount[0]['count']},indent=2))
+    
