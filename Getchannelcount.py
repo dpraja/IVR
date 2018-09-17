a846a42d03768468570f84dcf2ff81c34659ca43
dpraja <raja.pushpanathan@gmail.com>
2018-09-17 11:06:13 +0530

    Update Getchannelcount.py

--- Getchannelcount.py
+++ Getchannelcount.py
@@ -13,13 +13,13 @@ def Getchannelcounts(request):
     channelchatbot = json.loads(dbget("select count(*) from ivr_resevation where arrival_date between  '"+date_from+"' and  '"+date_to+"' and channel in ('CHATBOT')"))
     print(channelchatbot)
 
-    channelivr = json.loads(dbget("select count(*) from ivr_resevation where arrival_date between  '"+date_from+"' and  '"+date_to+"' and channel in ('IVR')"))
-    print(channelivr)
+    #channelivr = json.loads(dbget("select count(*) from ivr_resevation where arrival_date between  '"+date_from+"' and  '"+date_to+"' and channel in ('IVR')"))
+    #print(channelivr)
 
     ivrcount = json.loads(dbget("select count (*) from ivr_room_customer_booked where customer_arrival_date between  '"+date_from+"' and  '"+date_to+"' and channel in ('IVR')"))
     print(ivrcount)
     return(json.dumps({"Return":"Record Retrieved Sucessfully","Return_Code":"RTS","Status": "Success",
                       "Status_Code": "200","chatbotcount":channelchatbot[0]['count'],
-                       "ivrchannelcount":channelivr[0]['count'] + ivrcount[0]['count']},indent=2))
+                       "ivrchannelcount":channelivr[0]['count'] },indent=2))
     
     
