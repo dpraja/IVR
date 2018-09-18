d8d38a6155d50f1338cc571789b3eba16ba347b7
dpraja <raja.pushpanathan@gmail.com>
2018-09-18 16:30:52 +0530

    Update AddDiscount.py

--- AddDiscount.py
+++ AddDiscount.py
@@ -6,7 +6,11 @@ def adddiscount(request):
     res = request.json
     print(res)
     e = { k : v for k,v in res.items() if k in ('business_id','room_type')}
-    d = { k : v for k,v in res.items() if k not in ('business_id','room_type')}
+    d = { k : v for k,v in res.items() if k not in ('business_id','room_type','room_date_st','room_date_ed')}
+    st_d = res['room_date_st']
+    ed_d = res['room_date_ed']
+    st_d = datetime.datetime.strptime(st_d,'%Y-%m-%d').date()
+    ed_d = datetime.datetime.strptime(ed_d,'%Y-%m-%d').date()    
     #print(e)
     #print(d)
     f = { k : v for k,v in res.items() if k in ('room_date')}
@@ -15,11 +19,13 @@ def adddiscount(request):
     print("id_data",id_data,type(id_data))
     str_id = ''
     for i in id_data:
-       sql = json.loads(dbget("select count(*) from extranet_discount where id in ("+str(i['id'])+") and room_date='"+d['room_date']+"' "))
+      while st_d <= ed_d :
+       sql = json.loads(dbget("select count(*) from extranet_discount where id in ("+str(i['id'])+") and room_date='"+str(st_d)+"' "))
        print(sql[0]['count'])
        if sql[0]['count'] == 0:
           try:
-            d['id'] = i['id']   
+            d['id'] = i['id']
+            d['room_date'] = st_d
             print(gensql('insert','extranet_discount',d))
             print("inserted")
           except:
@@ -32,5 +38,6 @@ def adddiscount(request):
           except:
             return(json.dumps({"ServiceStatus":"Failure","ServiceMessage":"Failure"},indent=2))
             print("record already inserted")
+       st_d += datetime.timedelta(days=1)    
     return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Record Inserted"},indent=2))   
     
