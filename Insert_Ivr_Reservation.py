c44ecbaddef6f84a4f0a3550793e902261c489ef
dpraja <raja.pushpanathan@gmail.com>
2018-09-18 13:02:17 +0530

    Update Insert_Ivr_Reservation.py

--- Insert_Ivr_Reservation.py
+++ Insert_Ivr_Reservation.py
@@ -4,7 +4,9 @@ import random
 
 def Insert_Ivr_Reservation(request):
     d = request.json
-    gensql('insert','ivr_resevation',d)
     confirmation = (random.randint(1000,9999))
+    d['confirmation_number'] = confirmation
+    gensql('insert','ivr_resevation',d)
+    confirmation = d.get('confirmation_number')
     a = {"Return":"Record Inserted Successfully","ReturnCode":"RIS","ReturnMessage":"Success","Confirmation_Number":confirmation}
     return(json.dumps(a,indent=2))
