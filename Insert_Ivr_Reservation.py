a84ac8d07db8e5723871f594361133846cf53599
dpraja <raja.pushpanathan@gmail.com>
2018-09-21 15:15:29 +0530

    Update Insert_Ivr_Reservation.py

--- Insert_Ivr_Reservation.py
+++ Insert_Ivr_Reservation.py
@@ -10,3 +10,13 @@ def Insert_Ivr_Reservation(request):
     confirmation = d.get('confirmation_number')
     a = {"Return":"Record Inserted Successfully","ReturnCode":"RIS","ReturnMessage":"Success","Confirmation_Number":confirmation}
     return(json.dumps(a,indent=2))
+
+def Query_Reservation(request):
+    d = json.loads(dbget("select customer_mobile, customer_arrival_date, customer_depature_date,\
+                          customer_room_type, customer_room_rate,\
+                          customer_pickup_drop,customer_booked_date,customer_cc,customer_expirydate,\
+                          customer_amount,customer_booked_status,\
+                          customer_booked_status,customer_adult,customer_child,customer_no_of_rooms,\
+                          channel from ivr_room_customer_booked"))
+    print(d)
+    return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success","result":d},indent=2))
