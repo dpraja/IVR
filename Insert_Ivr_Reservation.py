6c6a8bd28999bb9ec074aeca47f40cc227c83f56
dpraja <raja.pushpanathan@gmail.com>
2018-09-27 16:03:46 +0530

    Update Insert_Ivr_Reservation.py

--- Insert_Ivr_Reservation.py
+++ Insert_Ivr_Reservation.py
@@ -12,11 +12,22 @@ def Insert_Ivr_Reservation(request):
     return(json.dumps(a,indent=2))
 
 def Query_Reservation(request):
+    '''
     d = json.loads(dbget("select customer_mobile, customer_arrival_date, customer_depature_date,\
                           customer_room_type, customer_room_rate,\
                           customer_pickup_drop,customer_booked_date,customer_cc,customer_expirydate,\
                           customer_amount,customer_booked_status,\
                           customer_booked_status,customer_adult,customer_child,customer_no_of_rooms,\
                           channel,customer_confirmation_number from ivr_room_customer_booked"))
+    '''
+    d = json.loads(dbget("SELECT * FROM public.ivr_room_customer_booked;"))
+    e = json.loads(dbget("select customer_name, customer_room_rate, customer_booking_confirmed, customer_booked_date, customer_menu_navigation,\
+                          customer_cc, customer_expirydate, customer_amount, customer_booked_status, id, customer_no_of_rooms, modification,\
+                          ivr_language, phone_number as customer_mobile, arrival_date as customer_arrival_date, depature_date as customer_depature_date,\
+                          adult as customer_adult, child as customer_child, roomtype as customer_room_type, pickup as customer_pickup_drop,\
+                          channel, confirmation_number as customer_confirmation_number, sms as send_sms, countrycode as cntry_code,\
+                          ivr_res_id as cus_id\
+                          from public.ivr_resevation"))
+    d = d+e
     print(d)
     return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success","result":d},indent=2))
