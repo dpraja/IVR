77976e4ef85eb86a611f075638004a30ec540979
dpraja <raja.pushpanathan@gmail.com>
2018-09-27 18:20:33 +0530

    Update Getreservationcancelmodification.py

--- Getreservationcancelmodification.py
+++ Getreservationcancelmodification.py
@@ -62,14 +62,11 @@ def GetBookingConfirmation(request):
 
     channel_bookingcount = json.loads(dbget("select count(confirmation_number) from public.ivr_resevation where arrival_date between  '"+date_from+"' and  '"+date_to+"'"))
     print(channel_bookingcount)
-    totalvalue = ivreservationcount[0]['count'] + channel_count[0]['count'] + sql_value[0]['count'] + channel_bookingcount[0]['count']
-    bookingperc = (ivreservationcount[0]['count'] + channel_count[0]['count']) * 100 /totalvalue
-    conperc = (sql_value[0]['count'] + channel_bookingcount[0]['count']) * 100/totalvalue
     
     json_input = [
-                   {"title":"Booking","value":ivreservationcount[0]['count'] + channel_count[0]['count'],"percentage":bookingperc },
+                   {"title":"Bookingcount","value":ivreservationcount[0]['count'] + channel_count[0]['count'] },
                    
-                   {"title":"Confirmation","value":sql_value[0]['count'] + channel_bookingcount[0]['count'],"percentage":conperc}
+                   {"title":"Confirmationcount","value":sql_value[0]['count'] + channel_bookingcount[0]['count']}
                    ]
   
    # json_input = {
@@ -91,13 +88,10 @@ def Getsmscount(request):
     ivrsmscount = json.loads(dbget("select count(*) from ivr_room_customer_booked where customer_arrival_date between '"+date_from+"' and '"+date_to+"' and send_sms in ('success')"))
     print(ivrsmscount)
     channelsmscount = json.loads(dbget("select count(*) from ivr_resevation where arrival_date between  '"+date_from+"' and  '"+date_to+"' and sms in ('success')"))
-    totalvalue = ivreservationcount[0]['count'] + channel_count[0]['count'] + ivrsmscount[0]['count'] + channelsmscount[0]['count']
-    bookedperc = (ivreservationcount[0]['count'] + channel_count[0]['count']) * 100/totalvalue
-    deliverdperc = (ivrsmscount[0]['count'] + channelsmscount[0]['count']) * 100/totalvalue
     json_input = [
-                   {"title":"Booked","value":ivreservationcount[0]['count'] + channel_count[0]['count'],"percentage":bookedperc },
+                   {"title":"Booked","value":ivreservationcount[0]['count'] + channel_count[0]['count'] },
                    
-                   {"title":"Delievered","value":ivrsmscount[0]['count'] + channelsmscount[0]['count'],"percentage":deliverdperc}
+                   {"title":"Delievered","value":ivrsmscount[0]['count'] + channelsmscount[0]['count']}
                    ]
     return(json.dumps({"Return":"Record Retrieved Sucessfully","Return_Code":"RTS","Status": "Success","Status_Code": "200","Returnvalue":json_input},indent=2))
 
@@ -111,14 +105,12 @@ def GetLanguagecount(request):
     
     english_count = json.loads(dbget("select count(*) from public.ivr_resevation where arrival_date between  '"+date_from+"' and  '"+date_to+"'"))
     print(english_count)
-    totalvalue = arabic_count[0]['count'] + english_count[0]['count'] +  ivr_englishcount[0]['count']
-    arabicperc = (arabic_count[0]['count']) * 100/totalvalue
-    englishperc = (english_count[0]['count'] +  ivr_englishcount[0]['count'] ) * 100/totalvalue
     json_input = [
-                   {"title":"Arabic","value":arabic_count[0]['count'],"percentage":arabicperc },
+                   {"title":"Arabic","value":arabic_count[0]['count']  },
                    
-                   {"title":"English","value":english_count[0]['count'] +  ivr_englishcount[0]['count'],"percentage":englishperc }
-                   ]    return(json.dumps({"Return":"Record Retrieved Sucessfully","Return_Code":"RTS","Status": "Success","Status_Code": "200","Returnvalue":json_input},indent=2))
+                   {"title":"English","value":english_count[0]['count'] +  ivr_englishcount[0]['count'] }
+                   ]
+    return(json.dumps({"Return":"Record Retrieved Sucessfully","Return_Code":"RTS","Status": "Success","Status_Code": "200","Returnvalue":json_input},indent=2))
 def GetRoomOccupancy(request):
     date_from = request.json['arrival_from']
     date_to = request.json['arrival_to']
