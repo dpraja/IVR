8e62d22cfa8f95609aacfdae2c70d571ec42140d
dpraja <raja.pushpanathan@gmail.com>
2018-10-05 16:28:54 +0530

    Update RoomList.py

--- RoomList.py
+++ RoomList.py
@@ -22,17 +22,27 @@ def insertroomlist(request):
      return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Failure"},indent=2)) 
 
 def restriction(request):
-    res = request.json
-    print(res)
-    restric_count = json.loads(dbget("select count(*) as c from restriction where business_id='"+res['business_id']+"' "))
-    print(restric_count)
-    if restric_count[0]['c'] == 0:
-        gensql('insert','restriction',res)
-    else:    
-       a = { k : v for k,v in res.items() if v != '' if k  not in ('business_id')}
-       e = { k : v for k,v in res.items() if v != '' if k   in ('business_id')}
-       gensql('update','restriction',a,e)
-    
+    d = request.json
+    #gensql('insert','restriction',d)
+    print(d)
+    business_id = d['business_id']
+    room_id = json.loads(dbget("select room_id from configration where business_id='"+str(business_id)+"' "))
+    print(dbput("update extranet_availableroom set min_stay='"+d['min_stay']+"' where room_date='"+d['min_date']+"' \
+           and room_id in (""select room_id from configration where business_id='"+str(business_id)+"'" ") "))
+
+    print(dbput("update extranet_availableroom set max_stay='"+d['max_stay']+"' where room_date='"+d['max_date']+"' \
+           and room_id in (""select room_id from configration where business_id='"+str(business_id)+"'" ") "))
+
+    print(dbput("update extranet_availableroom set house_close='1' where room_date='"+d['house_close']+"' \
+           and room_id in (""select room_id from configration where business_id='"+str(business_id)+"'" ") "))
+
+    print(dbput("update extranet_availableroom set close_arrival='1' where room_date between '"+d['close_arrival_from']+"' and '"+d['close_arrival_to']+"' \
+           and room_id in (""select room_id from configration where business_id='"+str(business_id)+"'" ") "))
+
+    print(dbput("update extranet_availableroom set close_departure='1' where room_date between '"+d['close_departure_from']+"' and '"+d['close_departure_to']+"' \
+           and room_id in (""select room_id from configration where business_id='"+str(business_id)+"'" ") "))
+
+    print(room_id)
     return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success"},indent=2))
 
 def select_restriction(request):
