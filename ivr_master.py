249d5259472662a61441cfb343bf841f0056faee
dpraja <raja.pushpanathan@gmail.com>
2018-10-05 11:01:22 +0530

    Update ivr_master.py

--- ivr_master.py
+++ ivr_master.py
@@ -57,6 +57,13 @@ from Getreservationcancelmodification import GetCountryreservation
 from Getreservationcancelmodification import monthreservation
 from Getreservationcancelmodification import futurebooking
 from Getreservationcancelmodification import HistoryBooking
+
+from Inserttwilioreservation import Inserttwilioreservation
+from Inserttwilioreservation import InsertArrivalDeparture
+from Inserttwilioreservation import Modifytwilioreservation
+from Inserttwilioreservation import Canceltwilioreservation
+from Inserttwilioreservation import Smstwilioservice
+from Inserttwilioreservation import CheckConfirmation
 # add changes
 from RatesandAvailability import daterange
 from RoomList import restriction
@@ -329,6 +336,25 @@ def SelectInclusionsConfiguration_all():
 @app.route('/SelectExtrabed',methods=['GET'])
 def SelectExtrabed_all():
    return SelectExtrabed()
+@app.route('/Inserttwilioreservation',methods=['POST'])
+def reservation():
+   return Inserttwilioreservation(request)
+@app.route('/InsertArrivalDeparture',methods=['POST'])
+def InsertArrivalDeparture_all():
+   return InsertArrivalDeparture(request)
+
+@app.route('/Modifytwilioreservation',methods=['POST'])
+def Modifytwilioreservation_all():
+   return Modifytwilioreservation(request)
+@app.route('/Canceltwilioreservation',methods=['POST'])
+def Canceltwilioreservation_all():
+   return Canceltwilioreservation(request)
+@app.route('/Smstwilioservice',methods=['POST'])
+def Smstwilioservice_all():
+   return Smstwilioservice(request)
+@app.route('/CheckConfirmation',methods=['POST'])
+def CheckConfirmation_all():
+   return CheckConfirmation(request)
 if __name__ == "__main__":
   app.run(debug=True)
   #app.run(host="192.168.1.10",port=5000)
