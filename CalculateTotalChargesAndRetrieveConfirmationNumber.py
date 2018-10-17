01d173803f04720abc2386af0556ba7cab9a6d89
dpraja <raja.pushpanathan@gmail.com>
2018-10-17 11:01:14 +0530

    Update CalculateTotalChargesAndRetrieveConfirmationNumber.py

--- CalculateTotalChargesAndRetrieveConfirmationNumber.py
+++ CalculateTotalChargesAndRetrieveConfirmationNumber.py
@@ -2,21 +2,29 @@ from sqlwrapper import gensql,dbget
 import json
 import datetime
 from decimal import Decimal
+import math
 
 def calculatetotalcharges(request):
-    try:
-        tfn = request.json['TFN']
+    #try:
+        tfn = request.json['tfn_num']
+        dividen_list = []
+        last_list = []
+        sumval = 0
+        datelist_rate = []
+        datelist_amount = {}
+        rate_plan_list = []
         b_id = json.loads(dbget("select id from ivr_dialed_number where dialed_number='"+tfn+"' "))
         print(b_id[0]['id'])
         bi_id = json.loads(dbget("select business_id from ivr_hotel_list where id='"+str(b_id[0]['id'])+"' "))
         print(bi_id[0]['business_id'],type(bi_id[0]['business_id']))
-        customer_arrival_date = request.json["customer_arrival_date"]
-        customer_depature_date = request.json["customer_depature_date"]
-        customer_room_type = request.json["customer_room_type"]
-        customer_room_type = customer_room_type.title()
-        print("roomtype",customer_room_type)
-        customer_adult = request.json["customer_adult"]
-        customer_child = request.json["customer_child"]
+        customer_arrival_date = request.json["arrival_date"]
+        customer_depature_date = request.json["depature_date"]
+        customer_room_type = request.json["room_type"] # ROOM_ID
+        print(customer_room_type,type(customer_room_type))
+       # customer_room_type = customer_room_type.title()
+       # print("roomtype",customer_room_type)
+        customer_adult = request.json["adult"]
+        customer_child = request.json["child"]
         print("adults",customer_adult,type(customer_adult))
         d,e,d1,d2 = {},[],{},{}
         print(customer_arrival_date,customer_depature_date)
@@ -39,59 +47,95 @@ def calculatetotalcharges(request):
 
         customer_depature_date = year+'-'+customer_depature_date[0:2]+'-'+customer_depature_date[2:]
         
-        print("arrival",customer_arrival_date,"depature",customer_depature_date)    
-        result = json.loads(dbget("select * from extranet_room_list where \
-                                   room_type in ('"+customer_room_type+"') and business_id='"+bi_id[0]['business_id']+"' "))
-        print("res",result)
-        res = result[0]
-        print("res",res)
-        currency = res['standard_rate_currency']
+        print("arrival",customer_arrival_date,"depature",customer_depature_date,"roomid",customer_room_type,"businessid",bi_id[0]['business_id'])    # CONFIGRATION
+        sql = json.loads(dbget("select max_extra_bed.extrabed,extranet_availableroom.extra_adult_rate,extranet_availableroom.rate_plan_id,extranet_availableroom.room_date,extranet_availableroom.room_rate,configration.max_adults \
+                                    from configration \
+                                   join extranet_availableroom on extranet_availableroom.room_id = configration.room_id \
+                                   join max_extra_bed on max_extra_bed.extrabed_id = configration.maximum_extrabed_id \
+                                   where configration.room_id  = '"+str(customer_room_type)+"' and configration.business_id='"+bi_id[0]['business_id']+"'and extranet_availableroom.room_date between '"+str(customer_arrival_date)+"' and '"+str(customer_depature_date)+"'"))
+        #print("res",result)
+     
+        #available_rate = json.loads(dbget("select room_id,room_rate,room_date,rate_plan_id from extranet_availableroom where room_date between '"+str(customer_arrival_date)+"' and '"+str(customer_depature_date)+"' and room_id='"+str(customer_room_type)+"'"))
+        #print(available_rate)
+        s = 0
+        total_adult = int(customer_adult)
+        max_adult = int(sql[0]['max_adults'])
+        extra_bed = int(sql[0]['extrabed'])
+        extra_adult_rate = sql[0]['extra_adult_rate']
+        rooms_rate =  int(sql[0]['room_rate'])
+        
+        plan_rate = int(sql[0]['rate_plan_id'])
+
+        print("plan_rate",plan_rate)
+        total_beds = max_adult + extra_bed
+        total_rooms = total_adult / total_beds
+        total_rooms_count = math.ceil(total_rooms)
+        print(total_rooms_count)
+        
+    
+        sumva = 0
+        for i in sql:
+                arrival_date = datetime.datetime.strptime(customer_arrival_date, '%Y-%m-%d')
+                depature_date = datetime.datetime.strptime(customer_depature_date, '%Y-%m-%d')
+                conf_date = datetime.datetime.strptime(i['room_date'], '%Y-%m-%d')
+                print("conf_date",conf_date)
+                deltadates = depature_date - arrival_date
+                for x in range(deltadates.days + 1):
+                   #print(arrival_date + datetime.timedelta(i))
+                   datebetween = arrival_date + datetime.timedelta(x)
+                   if datebetween == conf_date and plan_rate == int(i['rate_plan_id']) :
+                      print(i["extra_adult_rate"],i['room_rate'],i['room_rate'])
+                      r1 = max_adult * total_rooms_count
+                      extra_price = (int(customer_adult) - r1) * int(i["extra_adult_rate"])
+                      price = total_rooms_count * int(i['room_rate'])
+                      total = price + extra_price                        
+                      sumva += total
+                      ##price1 = result1 * int(i['room_rate'])
+                      
+                      #price2 = (total_adult - result1) * (int(i["extra_adult_rate"])+int(i['room_rate']))   
+                      datelist_rate.append({"day":datebetween.strftime('%d %B'),"total":total})
+                      #print(datelist_rate)
+                   
+              
+        print(datelist_rate)
+        print("sumva",sumva)
+       
+        '''
+       
+        price1 = result1 * rooms_rate
+        price2 = (total_adult - result1) * (extra_adult_rate+rooms_rate)
+        print(price2,price1)
+        total_amout = price1 + price2
         arrival_date = datetime.datetime.strptime(customer_arrival_date, '%Y-%m-%d')
         depature_date = datetime.datetime.strptime(customer_depature_date, '%Y-%m-%d')
-        date_to = depature_date - datetime.timedelta(days=1)
-        print(date_to)
-        print(arrival_date,depature_date)
-        night = (depature_date - arrival_date).days
-        if night == 0:
-            night = 1
-        print("night",night,type(night))
-        #Total_amt = night * room_rate
-        #print(Total_amt,type(Total_amt))
-        result = json.loads(dbget("select room_rate from extranet_availableroom where id in(select id from extranet_room_list where business_id = '"+str(bi_id[0]['business_id'])+"' \
-                                   and room_type= '"+customer_room_type+"') and room_date between \
-                                   '"+customer_arrival_date+"' and '"+str(date_to)+"' "))
-        print("result",result)
-        Total_amt = 0
-        for rate in result:
-             print("rate",rate)
-             Total_amt += rate['room_rate']
-        print("total",Total_amt)
-        no_of_rooms = 1
-        customer_adult = int(customer_adult)
-        customer_child = int(customer_child)
-        total_pers = customer_adult+customer_child
-        if total_pers>4:
-            no_of_rooms = total_pers/4
-            str_count = str(no_of_rooms)
-            print("first",no_of_rooms)
-            a = str_count.split('.')
-            print("aaaa",a[1],type(a[1]))
-            if int(a[1]) != 0:  
-              no_of_rooms += 1
-              no_of_rooms = int(no_of_rooms)
-            #Decimal(no_of_rooms)
-              print("second",no_of_rooms)
+        deltadates = depature_date - arrival_date
+        for i in range(deltadates.days + 1):
+                 #print(arrival_date + datetime.timedelta(i))
+                 datebetween = arrival_date + datetime.timedelta(i)
+                 datebetween = datebetween.strftime('%d %B')
+                 dividen_list.append(datebetween)
+                 datelist_amount[""+str(datebetween)+""] = total_amout
+                 
+                 
+             #print(datelist_amount)
+        #s = 0
+        for k,v in datelist_amount.items():
+                     #name = "day"+""+str(s)+""
+                     #amounts = "amount"+""+str(s)+""
+                     #print(name)
+                     last_list.append({"day":k,"amount":v})
+                    # s += 1
+        #print(sumval)
+        print(last_list)
+            #print(dividen_list)
             
+        #print(x)
+        #price2 = 
+        #price2 = total_rooms_count *
         '''
-        if customer_adult > 2:
-           if customer_adult%2 > 0:
-              customer_adult += 1
-              no_of_rooms = customer_adult/2
-           else:
-              no_of_rooms = customer_adult/2
-        '''      
-        Total_amt = Total_amt * no_of_rooms
-        print(Total_amt)
-        return(json.dumps({"ServiceMessage":"Success","Total_Amount":Total_amt,"currency":currency,"no_of_rooms":int(no_of_rooms)}))
-    except:
-        return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Failure"}))
+        return(json.dumps({"ServiceMessage":"Success","Total_Amount":sumva,"date_amount":datelist_rate},indent=2))
+        
+        #return(json.dumps({"ServiceMessage":"Success","Total_Amount":total_amout,"date_month_amount":last_list},indent=2))
+        
+    #except:
+       # return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Failure"}))
