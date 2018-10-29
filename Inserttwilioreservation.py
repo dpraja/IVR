e43b00d6bbe6bf289ad0df749f5b795ebc4303fb
dpraja <raja.pushpanathan@gmail.com>
2018-10-29 17:03:11 +0530

    Update Inserttwilioreservation.py

--- Inserttwilioreservation.py
+++ Inserttwilioreservation.py
@@ -42,7 +42,7 @@ def Inserttwilioreservation(request):
     d['customer_depature_date'] = customer_depature_date
     d['customer_confirmation_number'] = confir
     d['modification'] = "No"
-    d['customer_booked_status'] = d['customer_booked_status'].title()
+    d['customer_booked_status'] = d['customer_booked_status'].lower()
     d['customer_room_type'] = roomtype.title()
     d['business_id'] = str(bi_id[0]['business_id'])
     d['ivr_language'] = request.json['ivr_language']
