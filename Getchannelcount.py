f749588aa4b3909115fd7b18ab01708bf33611ec
dpraja <raja.pushpanathan@gmail.com>
2018-10-30 11:08:55 +0530

    Update Getchannelcount.py

--- Getchannelcount.py
+++ Getchannelcount.py
@@ -8,24 +8,22 @@ def Getchannelcounts(request):
     
     date_from = request.json['arrival_from']
     date_to = request.json['arrival_to']
+    business_id = request.json['business_id']
 
-
-    channelchatbot = json.loads(dbget("select count(*) from ivr_resevation where arrival_date between  '"+date_from+"' and  '"+date_to+"' and channel in ('CHATBOT')"))
+    channelchatbot = json.loads(dbget("select count(*) from ivr_room_customer_booked where business_id = '"+business_id+"' and customer_arrival_date between  '"+date_from+"' and  '"+date_to+"' and channel in ('whatsapp')"))
     print(channelchatbot)
 
     #channelivr = json.loads(dbget("select count(*) from ivr_resevation where arrival_date between  '"+date_from+"' and  '"+date_to+"' and channel in ('IVR')"))
     #print(channelivr)
 
-    ivrcount = json.loads(dbget("select count (*) from ivr_room_customer_booked where customer_arrival_date between  '"+date_from+"' and  '"+date_to+"' and channel in ('IVR')"))
+    ivrcount = json.loads(dbget("select count (*) from ivr_room_customer_booked where business_id = '"+business_id+"' and customer_arrival_date between  '"+date_from+"' and  '"+date_to+"' and channel in ('IVR')"))
     print(ivrcount)
 
     
-    totalvalue = channelchatbot[0]['count'] + ivrcount[0]['count']
-    chatperc = channelchatbot[0]['count'] * 100/totalvalue
-    ivrperc = ivrcount[0]['count'] * 100/ totalvalue
+
     
-    json_input = [{"title":"Chatbot","value":channelchatbot[0]['count'],"percentage":chatperc},
-                  {"title":"Ivr","value":ivrcount[0]['count'],"percentage":ivrperc}
+    json_input = [{"title":"Chatbot","value":channelchatbot[0]['count']},
+                  {"title":"Ivr","value":ivrcount[0]['count']}
                    ]
     return(json.dumps({"Return":"Record Retrieved Sucessfully","Return_Code":"RTS","Status": "Success",
                       "Status_Code": "200","Returnvalue":json_input },indent=2))
