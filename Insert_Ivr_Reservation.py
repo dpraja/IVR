b8eb609d5277dea1a502b6c0a3b3fd9bf7fd4c1d
dpraja <raja.pushpanathan@gmail.com>
2018-11-01 16:57:47 +0530

    Update Insert_Ivr_Reservation.py

--- Insert_Ivr_Reservation.py
+++ Insert_Ivr_Reservation.py
@@ -13,16 +13,11 @@ def Insert_Ivr_Reservation(request):
 
 def Query_Reservation(request):
     b_id = request.json
-    d = json.loads(dbget("SELECT * FROM public.ivr_room_customer_booked where business_id='"+b_id['business_id']+"'"))
-    '''
-    e = json.loads(dbget("select customer_name, customer_room_rate, customer_booking_confirmed, customer_booked_date, customer_menu_navigation,\
-                          customer_cc, customer_expirydate, customer_amount, customer_booked_status, id, customer_no_of_rooms, modification,\
-                          ivr_language, phone_number as customer_mobile, arrival_date as customer_arrival_date, depature_date as customer_depature_date,\
-                          adult as customer_adult, child as customer_child, roomtype as customer_room_type, pickup as customer_pickup_drop,\
-                          channel, confirmation_number as customer_confirmation_number, sms as send_sms, countrycode as cntry_code,\
-                          ivr_res_id as cus_id\
-                          from public.ivr_resevation where  "))
-    '''                          
+    d = json.loads(dbget("SELECT ivr_room_customer_booked.*,ivr_hotel_list.* FROM \
+                          public.ivr_room_customer_booked join ivr_hotel_list on \
+                          ivr_room_customer_booked.business_id = ivr_hotel_list.business_id where \
+                          ivr_room_customer_booked.business_id='"+b_id['business_id']+"'"))
+                              
     
     #print(d)
     return(json.dumps({"ServiceStatus":"Success","ServiceMessage":"Success","result":d},indent=2))
@@ -30,7 +25,7 @@ def Query_Reservation(request):
 def Query_Rate_Per_day(request):
     confi_num = request.json['conf_num']
     b_id = request.json['business_id']
-    print(request.json)
+    #print(request.json)
     d = json.loads(dbget("select * from customer_rate_detail where \
                           business_id='"+b_id+"' and customer_confirmation_number='"+confi_num+"' "))
         
