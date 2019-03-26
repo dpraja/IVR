fc3265918e924e9b8739af2b6293fe220e63667d
dpraja <raja.pushpanathan@gmail.com>
2019-03-26 10:30:13 +0530

    add post and get method

--- gupshupproof.py
+++ gupshupproof.py
@@ -3,12 +3,17 @@ import json
 #import re
 from flask import Flask,request,jsonify
 def updategupshupreservation(request):
-    #try:
-        d=request.json
-        #customer_email = json.loads(dbget("select count(*) customer_email from ivr_room_customer_booked where customer_email ='"+str(d['customer_email'])+"'"))
-        dbput("update  ivr_room_customer_booked set id_proof='"+str(d['id_proof'])+"' where customer_mobile='"+str(d['customer_mobile'])+"'")
+    try:
+        if request.method == 'GET':
+            customer_mobile = request.args['customer_mobile']
+            id_proof = request.args['id_proof']
+        if request.method == 'POST':
+            customer_mobile = request.json['customer_mobile']
+            id_proof = request.json['id_proof']
+        
+        dbput("update  ivr_room_customer_booked set id_proof='"+str(id_proof)+"' where customer_mobile='"+(customer_mobile)+"'")
         print("mohan")
         return(json.dumps({"Message":"Record Updated Successfully","Message_Code":"RUS","Service_Status":"Success"},indent=4))
         
-    #except:
-        #return(json.dumps({"Message":"Recored Updated UnSuccessfully","Message_Code":"RUUS","Service":"Failure"},indent=4))
+    except:
+        return(json.dumps({"Message":"Recored Updated UnSuccessfully","Message_Code":"RUUS","Service":"Failure"},indent=4))
